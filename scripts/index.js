c4743ff7e32fe26d076b738ad4d1dff24ea4628e
Ashish Garg <mragarg@gmail.com>
2019-03-13 19:50:55 -0400

    Gets JSON data from GoT API with fetch

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,9 @@
-// Template
+const URL = "https://my-little-cors-proxy.herokuapp.com/https://anapioficeandfire.com/api/characters/";
+
+fetch(URL)
+    .then(function (response){ // Process the response so we can use it
+        return response.json();
+    })
+    .then(function (theActualData){ // Receive the Actual Data and do something
+        console.log(theActualData);
+    });
