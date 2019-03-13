c7ca1e5773483444fcf0f359fef68432f6e80e1d
Ashish Garg <mragarg@gmail.com>
2019-03-13 19:56:01 -0400

    Creates a function that takes the data from API and adds to an array

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,17 @@
 const URL = "https://my-little-cors-proxy.herokuapp.com/https://anapioficeandfire.com/api/characters/";
 
+let allCharactersArray = [];
+
+function accumulateCharacters(theActualData){ // Receive the Actual Data and do something
+    // console.log(theActualData);
+    allCharactersArray = [
+        ...allCharactersArray,
+        ...theActualData
+    ];
+}
+
 fetch(URL)
-    .then(function (response){ // Process the response so we can use it
+    .then(function (response){ // Process the response so we can get the data
         return response.json();
     })
-    .then(function (theActualData){ // Receive the Actual Data and do something
-        console.log(theActualData);
-    });
+    .then(accumulateCharacters);
